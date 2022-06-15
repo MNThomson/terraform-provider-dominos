cbed9d4b22d06248e4ec4ae6fe8bbab0e5f88e62
Max Thomson <info@mthomson.ca>
2022-06-15 15:17:52 -0700

    Provider + start on resourceOrder

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,76 +1,42 @@
 package provider
 
 import (
-	"strconv"
-	"strings"
+	"context"
+	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	_ "context"
+	_ "fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Provider exports the actual provider.
-func Provider() *schema.Provider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"email_address": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"first_name": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"last_name": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"phone_number": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"credit_card": &schema.Schema{
-				Type:     schema.TypeList,
-				Optional: true,
-				MaxItems: 1,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"number": &schema.Schema{
-							Type:      schema.TypeInt,
-							Required:  true,
-							Sensitive: true,
-						},
-						"cvv": &schema.Schema{
-							Type:      schema.TypeInt,
-							Required:  true,
-							Sensitive: true,
-						},
-						"date": &schema.Schema{
-							Type:      schema.TypeString,
-							Required:  true,
-							Sensitive: true,
-						},
-						"zip": &schema.Schema{
-							Type:      schema.TypeInt,
-							Required:  true,
-							Sensitive: true,
-						},
-					},
-				},
-			},
-		},
-		ResourcesMap: map[string]*schema.Resource{
-			"dominos_order": resourceOrder(),
-		},
-		DataSourcesMap: map[string]*schema.Resource{
-			"dominos_address":   dataSourceAddress(),
-			"dominos_store":     dataSourceStore(),
-			"dominos_menu":      dataSourceMenu(),
-			"dominos_menu_item": dataSourceMenuItem(),
-		},
-		ConfigureFunc: providerConfigure,
-	}
+// Ensure provider defined types fully satisfy framework interfaces
+var _ tfsdk.Provider = &provider{}
+
+// provider satisfies the tfsdk.Provider interface and usually is included
+// with all Resource and DataSource implementations.
+type provider struct {
+	// client can contain the upstream provider SDK or HTTP client used to
+	// communicate with the upstream service. Resource and DataSource
+	// implementations can then make calls using this client.
+	//
+	// TODO: If appropriate, implement upstream provider SDK or HTTP client.
+	// client vendorsdk.ExampleClient
+
+	// configured is set to true at the end of the Configure method.
+	// This can be used in Resource and DataSource implementations to verify
+	// that the provider was previously configured.
+	configured bool
+
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
 }
 
-type Config struct {
+type providerData struct {
 	FirstName        string
 	LastName         string
 	EmailAddr        string
@@ -82,30 +48,121 @@ type Config struct {
 	CardType         string
 }
 
-func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	config := Config{
-		FirstName:   d.Get("first_name").(string),
-		LastName:    d.Get("last_name").(string),
-		EmailAddr:   d.Get("email_address").(string),
-		PhoneNumber: d.Get("phone_number").(string),
+func (p *provider) Configure(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
+	var data providerData
+	diags := req.Config.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
-	if _, ok := d.GetOk("credit_card"); ok {
-		config.CreditCardNumber = int64(d.Get("credit_card.0.number").(int))
-		config.Cvv = int64(d.Get("credit_card.0.cvv").(int))
-		config.ExprDate = d.Get("credit_card.0.date").(string)
-		config.Zip = int64(d.Get("credit_card.0.zip").(int))
-		n := strconv.Itoa(int(config.CreditCardNumber))
+	data.CardType = "VISA"
 
-		if strings.HasPrefix(n, "3") {
-			config.CardType = "AMEX"
-		} else if strings.HasPrefix(n, "4") {
-			config.CardType = "VISA"
-		} else if strings.HasPrefix(n, "5") {
-			config.CardType = "MASTERCARD"
-		} else if strings.HasPrefix(n, "6") {
-			config.CardType = "DISCOVER"
+	p.configured = true
+}
+
+func (p *provider) GetResources(ctx context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
+	return map[string]tfsdk.ResourceType{
+		"dominos_order": resourceOrderType{},
+	}, nil
+}
+
+func (p *provider) GetDataSources(ctx context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
+	return map[string]tfsdk.DataSourceType{
+		"dominos_address": dataSourceAddressType{},
+		// "dominos_store":     dataSourceStoreType{},
+		// "dominos_menu":      dataSourceMenuType{},
+		// "dominos_menu_item": dataSourceMenuItemType{},
+	}, nil
+}
+
+func (p *provider) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"email_address": {
+				// MarkdownDescription: "Example provider attribute",
+				Optional: true,
+				Type:     types.StringType,
+			},
+			"first_name": {
+				// MarkdownDescription: "Example provider attribute",
+				Optional: true,
+				Type:     types.StringType,
+			},
+			"last_name": {
+				// MarkdownDescription: "Example provider attribute",
+				Optional: true,
+				Type:     types.StringType,
+			},
+			"phone_number": {
+				// MarkdownDescription: "Example provider attribute",
+				Optional: true,
+				Type:     types.StringType,
+			},
+			"credit_card": {
+				// MarkdownDescription: "Example provider attribute",
+				Optional: true,
+				Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
+					"number": {
+						Type:      types.Int64Type,
+						Required:  true,
+						Sensitive: true,
+					},
+					"cvv": {
+						Type:      types.Int64Type,
+						Required:  true,
+						Sensitive: true,
+					},
+					"date": {
+						Type:      types.Int64Type,
+						Required:  true,
+						Sensitive: true,
+					},
+					"zip": {
+						Type:      types.StringType,
+						Required:  true,
+						Sensitive: true,
+					},
+				}),
+			},
+		},
+	}, nil
+}
+
+func New(version string) func() tfsdk.Provider {
+	return func() tfsdk.Provider {
+		return &provider{
+			version: version,
 		}
 	}
-	return &config, nil
+}
+
+// convertProviderType is a helper function for NewResource and NewDataSource
+// implementations to associate the concrete provider type. Alternatively,
+// this helper can be skipped and the provider type can be directly type
+// asserted (e.g. provider: in.(*provider)), however using this can prevent
+// potential panics.
+func convertProviderType(in tfsdk.Provider) (provider, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	p, ok := in.(*provider)
+
+	if !ok {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. This is always a bug in the provider code and should be reported to the provider developers.", p),
+		)
+		return provider{}, diags
+	}
+
+	if p == nil {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			"While creating the data source or resource, an unexpected empty provider instance was received. This is always a bug in the provider code and should be reported to the provider developers.",
+		)
+		return provider{}, diags
+	}
+
+	return *p, diags
 }
--- internal/provider/resource_order.go
+++ internal/provider/resource_order.go
@@ -1,289 +1,172 @@
 package provider
 
 import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"strings"
-	"time"
+	"context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
-func resourceOrder() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceOrderCreate,
-		Read:   resourceOrderRead,
-		Delete: resourceOrderDelete,
-		Schema: map[string]*schema.Schema{
-			"address_api_object": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"item_codes": &schema.Schema{
-				Type:     schema.TypeList,
-				Required: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-				ForceNew: true,
-			},
-			"store_id": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew: true,
-			},
-			"price_only": &schema.Schema{
-				Type:     schema.TypeBool,
+// Ensure provider defined types fully satisfy framework interfaces
+var _ tfsdk.ResourceType = resourceOrderType{}
+var _ tfsdk.Resource = resourceOrder{}
+var _ tfsdk.ResourceWithImportState = resourceOrder{}
+
+type resourceOrderType struct{}
+
+func (t resourceOrderType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Example resource",
+		Attributes: map[string]tfsdk.Attribute{
+			"address_api_object": {
+				// MarkdownDescription: "Example configurable attribute",
 				Optional: true,
-				Default:  false,
-				ForceNew: true,
+				Type:     types.StringType,
 			},
-			"total_price": &schema.Schema{
-				Type:     schema.TypeFloat,
+			"item_codes": {
+				// MarkdownDescription: "Example configurable attribute",
+				Optional: true,
+				Type: types.ListType{
+					ElemType: types.Int64Type,
+				},
+			},
+			"store_id": {
+				// MarkdownDescription: "Example configurable attribute",
+				Optional: true,
+				Type:     types.Int64Type,
+			},
+			"price_only": {
+				// MarkdownDescription: "Example configurable attribute",
+				Optional: true,
+				Type:     types.BoolType,
+			},
+			"total_price": {
 				Computed: true,
+				// MarkdownDescription: "Example identifier",
+				PlanModifiers: tfsdk.AttributePlanModifiers{
+					tfsdk.UseStateForUnknown(),
+				},
+				Type: types.StringType,
 			},
 		},
-	}
+	}, nil
 }
 
-func resourceOrderCreate(d *schema.ResourceData, m interface{}) error {
-	var client = &http.Client{Timeout: 10 * time.Second}
-	store_id := d.Get("store_id").(string)
-	address_obj := make(map[string]interface{})
-	err := json.NewDecoder(strings.NewReader(d.Get("address_api_object").(string))).Decode(&address_obj)
-	if err != nil {
-		return err
-	}
-	order_data := map[string]interface{}{
-		"Address":               address_obj,
-		"Coupons":               []string{},
-		"CustomerID":            "",
-		"Extension":             "",
-		"OrderChannel":          "OLO",
-		"OrderID":               "",
-		"NoCombine":             true,
-		"OrderMethod":           "Web",
-		"OrderTaker":            nil,
-		"Payments":              []map[string]interface{}{map[string]interface{}{"Type": "Cash"}},
-		"Products":              []map[string]interface{}{},
-		"Market":                "",
-		"Currency":              "",
-		"ServiceMethod":         "Delivery",
-		"Tags":                  map[string]string{},
-		"Version":               "1.0",
-		"SourceOrganizationURI": "order.dominos.com",
-		"LanguageCode":          "en",
-		"Partners":              map[string]string{},
-		"NewUser":               true,
-		"metaData":              map[string]string{},
-		"Amounts":               map[string]string{},
-		"BusinessDate":          "",
-		"EstimatedWaitMinutes":  "",
-		"PriceOrderTime":        "",
-		"AmountBreakdown":       map[string]string{},
-		"StoreID":               store_id,
-	}
+func (t resourceOrderType) NewResource(ctx context.Context, in tfsdk.Provider) (tfsdk.Resource, diag.Diagnostics) {
+	provider, diags := convertProviderType(in)
 
-	menuapi, err := getMenuApiObject(fmt.Sprintf("https://order.dominos.com/power/store/%s/menu?lang=en&structured=true", store_id), client)
-	if err != nil {
-		return err
-	}
-	codes := d.Get("item_codes").([]interface{})
-	for i := range codes {
-		variant := menuapi["Variants"].(map[string]interface{})[codes[i].(string)].(map[string]interface{})
-		variant["ID"] = 1
-		variant["isNew"] = true
-		variant["Qty"] = 1
-		variant["AutoRemove"] = false
-		order_data["Products"] = append(order_data["Products"].([]map[string]interface{}), variant)
-	}
-	log.Printf("order data: %#v", order_data)
-	config := m.(*Config)
-	order_data["Email"] = config.EmailAddr
-	order_data["FirstName"] = config.FirstName
-	order_data["LastName"] = config.LastName
-	order_data["Phone"] = config.PhoneNumber
-	val_bytes, err := json.Marshal(map[string]interface{}{"Order": order_data})
-	if err != nil {
-		return err
-	}
-	val_req, err := http.NewRequest("POST", "https://order.dominos.com/power/price-order", strings.NewReader(string(val_bytes)))
-	if err != nil {
-		return err
-	}
-	val_req.Header.Set("Referer", "https://order.dominos.com/en/pages/order/")
-	val_req.Header.Set("Content-Type", "application/json")
-	dumpreq, err := httputil.DumpRequest(val_req, true)
-	if err != nil {
-		return err
-	}
-	log.Printf("http request:  %#v", string(dumpreq))
-	val_rsp, err := client.Do(val_req)
-	if err != nil {
-		return err
-	}
-	dumprsp, err := httputil.DumpResponse(val_rsp, true)
-	if err != nil {
-		return err
-	}
-	log.Printf("http response: %#v", string(dumprsp))
-	validate_response_obj := make(map[string]interface{})
-	err = json.NewDecoder(val_rsp.Body).Decode(&validate_response_obj)
-	if validate_response_obj["Status"].(float64) == -1 {
-		return fmt.Errorf("The Dominos API didn't like this request: %#v", validate_response_obj["StatusItems"])
-	}
-	for k, v := range validate_response_obj["Order"].(map[string]interface{}) {
-		if list, ok := v.([]interface{}); !ok || len(list) > 0 {
-			order_data[k] = v
-		}
-	}
-
-	price := order_data["Amounts"].(map[string]interface{})["Customer"]
-	d.Set("total_price", price.(float64))
-	if d.Get("price_only").(bool) {
-		return nil
-	}
-
-	if config.CreditCardNumber != 0 {
-		order_data["Payments"] = []map[string]interface{}{map[string]interface{}{
-			"Type":         "CreditCard",
-			"Expiration":   config.ExprDate,
-			"Amount":       order_data["Amounts"].(map[string]interface{})["Customer"],
-			"CardType":     config.CardType,
-			"Number":       config.CreditCardNumber,
-			"SecurityCode": config.Cvv,
-			"PostalCode":   config.Zip,
-		}}
-	}
-
-	order_bytes, err := json.Marshal(map[string]interface{}{"Order": order_data})
-	if err != nil {
-		return err
-	}
-	order_req, err := http.NewRequest("POST", "https://order.dominos.com/power/place-order", strings.NewReader(string(order_bytes)))
-	if err != nil {
-		return err
-	}
-	order_req.Header.Set("Referer", "https://order.dominos.com/en/pages/order/")
-	order_req.Header.Set("Content-Type", "application/json")
-
-	dump_order_req, err := httputil.DumpRequest(order_req, true)
-	if err != nil {
-		return err
-	}
-	log.Printf("http request:  %#v", string(dump_order_req))
-	order_rsp, err := client.Do(order_req)
-	if err != nil {
-		return err
-	}
-	dump_order_rsp, err := httputil.DumpResponse(order_rsp, true)
-	if err != nil {
-		return err
-	}
-	log.Printf("http response: %#v", string(dump_order_rsp))
-	order_response_obj := make(map[string]interface{})
-	err = json.NewDecoder(order_rsp.Body).Decode(&order_response_obj)
-	if err != nil {
-		return err
-	}
-	orderid := order_response_obj["Order"].(map[string]interface{})["StoreOrderID"]
-	d.SetId(orderid.(string))
-	return resourceOrderRead(d, m)
+	return resourceOrder{
+		provider: provider,
+	}, diags
 }
 
-func getOrderStatus(client *http.Client, phonenumber, orderid string) (string, error) {
-	order, err := getOrder(client, phonenumber, orderid)
-	log.Printf("[DEBUG] order: %#v", order)
-	return order.OrderStatus, err
+type exampleResourceData struct {
+	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
+	Id                    types.String `tfsdk:"id"`
 }
 
-type Envelope struct {
-	XMLName xml.Name `xml:"Envelope"`
-	Body    Body     `xml:"Body"`
+type resourceOrder struct {
+	provider provider
 }
 
-type Body struct {
-	XMLName         xml.Name        `xml:"Body"`
-	TrackerResponse TrackerResponse `xml:"GetTrackerDataResponse"`
-}
+func (r resourceOrder) Create(ctx context.Context, req tfsdk.CreateResourceRequest, resp *tfsdk.CreateResourceResponse) {
+	var data exampleResourceData
 
-type TrackerResponse struct {
-	XMLName       xml.Name      `xml:"GetTrackerDataResponse"`
-	OrderStatuses OrderStatuses `xml:"OrderStatuses"`
-}
+	diags := req.Config.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
 
-type OrderStatuses struct {
-	XMLName     xml.Name      `xml:"OrderStatuses"`
-	OrderStatus []OrderStatus `xml:"OrderStatus"`
-}
-
-type OrderStatus struct {
-	XMLName          xml.Name `xml:"OrderStatus"`
-	OrderStatus      string   `xml:"OrderStatus"`
-	OrderDescription string   `xml:"OrderDescription"`
-	OrderID          string   `xml:"OrderID"`
-	StartTime        string   `xml:"StartTime"`
-	ParsedStartTime  time.Time
-}
-
-func getOrder(client *http.Client, phonenumber, orderid string) (*OrderStatus, error) {
-	// hm.  easy to fetch, hard to process.
-	// grab out "soap:Envelope.soap:Body.GetTrackerDataResponse.OrderStatuses.[OrderStatus where OrderID = orderid]
-	r, err := client.Get(fmt.Sprintf("https://trkweb.dominos.com/orderstorage/GetTrackerData?Phone=%s", phonenumber))
-	if err != nil {
-		return nil, err
+	if resp.Diagnostics.HasError() {
+		return
 	}
-	defer r.Body.Close()
-	resp := Envelope{}
-	b, err := ioutil.ReadAll(r.Body)
-	log.Printf("[DEBUG] Response from tracker: %s", b)
-	if err != nil {
-		return nil, err
-	}
-	err = xml.Unmarshal(b, &resp)
-	order := resp.Body.TrackerResponse.OrderStatuses.OrderStatus
-	for i := range order {
-		if order[i].OrderID == orderid {
-			t, err := time.Parse("2006-01-02T15:04:05", order[i].StartTime)
-			if err != nil {
-				return nil, err
-			}
-			order[i].ParsedStartTime = t
-			return &order[i], nil
-		}
-	}
-	return nil, fmt.Errorf("Could not find order ID %s in tracker.", orderid)
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// example, err := d.provider.client.CreateExample(...)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	//     return
+	// }
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+	data.Id = types.String{Value: "example-id"}
+
+	// write logs using the tflog package
+	// see https://pkg.go.dev/github.com/hashicorp/terraform-plugin-log/tflog
+	// for more information
+	tflog.Trace(ctx, "created a resource")
+
+	diags = resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
 }
 
-func resourceOrderRead(d *schema.ResourceData, m interface{}) error {
-	config := m.(*Config)
-	client := &http.Client{Timeout: 10 * time.Second}
-	var status string
-	var err error
-	for ; err == nil && status != "Complete"; status, err = getOrderStatus(client, config.PhoneNumber, d.Id()) {
-		time.Sleep(10 * time.Second)
-		log.Printf("[DEBUG] checking tracker...")
+func (r resourceOrder) Read(ctx context.Context, req tfsdk.ReadResourceRequest, resp *tfsdk.ReadResourceResponse) {
+	var data exampleResourceData
+
+	diags := req.State.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
 	}
-	if err != nil {
-		return err
-	}
-	order, err := getOrder(client, config.PhoneNumber, d.Id())
-	if err != nil {
-		return err
-	}
-	if time.Now().Sub(order.ParsedStartTime).Hours() > 12 {
-		d.SetId("")
-	}
-	return nil
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// example, err := d.provider.client.ReadExample(...)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
+	//     return
+	// }
+
+	diags = resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
 }
 
-func resourceOrderDelete(d *schema.ResourceData, m interface{}) error {
-	d.SetId("")
-	return nil
+func (r resourceOrder) Update(ctx context.Context, req tfsdk.UpdateResourceRequest, resp *tfsdk.UpdateResourceResponse) {
+	var data exampleResourceData
+
+	diags := req.Plan.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// example, err := d.provider.client.UpdateExample(...)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	//     return
+	// }
+
+	diags = resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+}
+
+func (r resourceOrder) Delete(ctx context.Context, req tfsdk.DeleteResourceRequest, resp *tfsdk.DeleteResourceResponse) {
+	var data exampleResourceData
+
+	diags := req.State.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// example, err := d.provider.client.DeleteExample(...)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
+	//     return
+	// }
+}
+
+func (r resourceOrder) ImportState(ctx context.Context, req tfsdk.ImportResourceStateRequest, resp *tfsdk.ImportResourceStateResponse) {
+	tfsdk.ResourceImportStatePassthroughID(ctx, tftypes.NewAttributePath().WithAttributeName("id"), req, resp)
 }
