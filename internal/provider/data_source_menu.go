e280690e64e0e390330eee3190227011c13360ba
Max Thomson <info@mthomson.ca>
2022-06-15 22:09:43 -0700

    Partial Menu Item data source

--- internal/provider/data_source_menu.go
+++ internal/provider/data_source_menu.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,41 +11,97 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func dataSourceMenu() *schema.Resource {
-	return &schema.Resource{
-		Read: resourceMenuRead,
-		Schema: map[string]*schema.Schema{
-			"store_id": &schema.Schema{
-				Type:     schema.TypeString,
+// Ensure provider defined types fully satisfy framework interfaces
+var _ tfsdk.DataSourceType = dataSourceMenuType{}
+var _ tfsdk.DataSource = dataSourceMenu{}
+
+type dataSourceMenuType struct{}
+
+func (t dataSourceMenuType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "Example data source",
+		Attributes: map[string]tfsdk.Attribute{
+			"store_id": {
+				Type:     types.StringType,
 				Required: true,
 			},
-			"menu": &schema.Schema{
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"code": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"price_cents": &schema.Schema{
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-					},
+			"menu": {
+				Type: types.ListType{
+					ElemType: types.StringType,
 				},
+				Computed: true,
+				Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
+					"name": {
+						Type:     types.Int64Type,
+						Computed: true,
+					},
+					"code": {
+						Type:     types.StringType,
+						Computed: true,
+					},
+					"price_cents": {
+						Type:     types.Int64Type,
+						Computed: true,
+					},
+				}),
 			},
 		},
-	}
+	}, nil
 }
 
+func (t dataSourceMenuType) NewDataSource(ctx context.Context, in tfsdk.Provider) (tfsdk.DataSource, diag.Diagnostics) {
+	provider, diags := convertProviderType(in)
+
+	return dataSourceMenu{
+		provider: provider,
+	}, diags
+}
+
+type dataSourceMenuData struct {
+	StoreID         types.Int64 `tfsdk:"store_id"`
+	DeliveryMinutes types.Int64 `tfsdk:"delivery_minutes"`
+}
+
+type dataSourceMenu struct {
+	provider provider
+}
+
+func (d dataSourceMenu) Read(ctx context.Context, req tfsdk.ReadDataSourceRequest, resp *tfsdk.ReadDataSourceResponse) {
+	var data dataSourceMenuData
+
+	diags := req.Config.Get(ctx, &data)
+
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var client = &http.Client{Timeout: 10 * time.Second}
+
+	menuitems, err := getAllMenuItems(fmt.Sprintf("https://order.dominos.com/power/store/%s/menu?lang=en&structured=true", data.StoreID), client)
+	if err != nil {
+		log.Fatalf("Cannot get all menu items")
+	}
+	menu := make([]map[string]interface{}, 0, len(menuitems))
+	for i := range menuitems {
+		menu = append(menu, map[string]interface{}{"name": menuitems[i].Name, "code": menuitems[i].Code, "price_cents": menuitems[i].PriceCents})
+	}
+
+	log.Printf("len menu: %d", len(menu))
+	data.StoreID.Value, _ = strconv.ParseInt(stores[0].StoreID, 10, 64)
+	data.StoreID.Null = false //TODO: Set properly
+
+	diags = resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+}
+
+/*
 func resourceMenuRead(d *schema.ResourceData, m interface{}) error {
 	var client = &http.Client{Timeout: 10 * time.Second}
 	store_id := d.Get("store_id").(string)
@@ -63,7 +120,7 @@ func resourceMenuRead(d *schema.ResourceData, m interface{}) error {
 	d.SetId(store_id)
 	return nil
 }
-
+*/
 type MenuItem struct {
 	Code       string
 	Name       string
--- internal/provider/data_source_menu_item.go
+++ internal/provider/data_source_menu_item.go
@@ -1,54 +1,117 @@
 package provider
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func dataSourceMenuItem() *schema.Resource {
-	return &schema.Resource{
-		Read: resourceMenuItemRead,
-		Schema: map[string]*schema.Schema{
-			"store_id": &schema.Schema{
-				Type:     schema.TypeString,
+// Ensure provider defined types fully satisfy framework interfaces
+var _ tfsdk.DataSourceType = dataSourceMenuItemType{}
+var _ tfsdk.DataSource = dataSourceMenuItem{}
+
+type dataSourceMenuItemType struct{}
+
+func (t dataSourceMenuItemType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		MarkdownDescription: "Example data source",
+		Attributes: map[string]tfsdk.Attribute{
+			"store_id": {
+				Type:     types.Int64Type,
 				Required: true,
 			},
-			"query_string": &schema.Schema{
-				Type:     schema.TypeList,
-				Required: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
+			"query_string": {
+				Type: types.ListType{
+					ElemType: types.StringType,
 				},
+				Required: true,
 			},
-			"matches": &schema.Schema{
-				Type:     schema.TypeList,
+			"matches": {
 				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"code": &schema.Schema{
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"price_cents": &schema.Schema{
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
+				Attributes: tfsdk.SingleNestedAttributes(map[string]tfsdk.Attribute{
+					"name": {
+						Type: types.StringType,
 					},
-				},
+					"code": {
+						Type: types.StringType,
+					},
+					"price_cents": {
+						Type: types.Int64Type,
+					},
+				}),
 			},
 		},
-	}
+	}, nil
 }
 
+func (t dataSourceMenuItemType) NewDataSource(ctx context.Context, in tfsdk.Provider) (tfsdk.DataSource, diag.Diagnostics) {
+	provider, diags := convertProviderType(in)
+
+	return dataSourceMenuItem{
+		provider: provider,
+	}, diags
+}
+
+type dataSourceMenuItemData struct {
+	StoreID     types.String   `tfsdk:"store_id"`
+	QueryString []interface{}  `tfsdk:"query_string"`
+	Matches     []*matchesData `tfsdk:"matches"`
+}
+
+type matchesData struct {
+	Name       string `tfsdk:"name"`
+	Code       string `tfsdk:"code"`
+	PriceCents int64  `tfsdk:"price_cents"`
+}
+
+type dataSourceMenuItem struct {
+	provider provider
+}
+
+func (d dataSourceMenuItem) Read(ctx context.Context, req tfsdk.ReadDataSourceRequest, resp *tfsdk.ReadDataSourceResponse) {
+	var data dataSourceMenuItemData
+
+	diags := req.Config.Get(ctx, &data)
+
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var client = &http.Client{Timeout: 10 * time.Second}
+	menuitems, err := getAllMenuItems(fmt.Sprintf("https://order.dominos.com/power/store/%s/menu?lang=en&structured=true", data.StoreID.Value), client)
+	if err != nil {
+		log.Fatalf("Cannot get all menu items")
+	}
+	// menu := make([]map[string]interface{}, 0, len(menuitems))
+	queries := data.QueryString.([]interface{})
+Menu:
+	for i := range menuitems {
+		for j := range queries {
+			if !strings.Contains(strings.ToLower(menuitems[i].Name), strings.ToLower(queries[j].(string))) {
+				continue Menu
+			}
+		}
+		data.Matches = append(data.Matches, &matchesData{Name: menuitems[i].Name, Code: menuitems[i].Code, PriceCents: menuitems[i].PriceCents})
+	}
+	// data.Matches = menu
+	// data.Matches. = false //TODO: Set properly
+
+	log.Printf("len menu: %d", len(data.Matches))
+
+	diags = resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+}
+
+/*
 func resourceMenuItemRead(d *schema.ResourceData, m interface{}) error {
 	var client = &http.Client{Timeout: 10 * time.Second}
 	store_id := d.Get("store_id").(string)
@@ -74,3 +137,5 @@ Menu:
 	d.SetId(store_id)
 	return nil
 }
+
+*/
